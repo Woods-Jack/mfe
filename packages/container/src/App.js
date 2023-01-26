1a86729730db0827dc00431415e5d37189868461
Jack Woods <woodsjack1999@gmail.com>
2023-01-26 16:23:34 +0000

    Arbitrary container update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 // Add a comment so there are changes to the container
+// Add another comment here lol
 export default () => {
   return <div>
     <h1>Container test...</h1>
